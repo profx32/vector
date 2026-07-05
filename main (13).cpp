7ac1c803eb6d39b5c7d2d3c2a476bf67d87606d4
Hashim Hatamleh <profx1324@gmail.com>
2026-07-05 13:21:59 +0300

    Add files via upload

---
+++ main (13).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+     vector<int>v1={1,2,3,4,5,6,7,8,9};
+     // v1.insert(v1.begin(),9);
+      cout << v1.size() << endl;
+      cout << v1.capacity() << endl;
+
+    return 0;
+}
